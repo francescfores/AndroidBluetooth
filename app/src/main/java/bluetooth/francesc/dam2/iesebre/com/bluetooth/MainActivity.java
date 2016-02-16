97d46c616661bcbdada7661f10c96d2092ad2530
Francesc Fores <francescfores@iesebre.com>
2016-02-16 19:56:17 +0100

    Check bluetooth paired

--- app/src/main/java/bluetooth/francesc/dam2/iesebre/com/bluetooth/MainActivity.java
+++ app/src/main/java/bluetooth/francesc/dam2/iesebre/com/bluetooth/MainActivity.java
@@ -2,31 +2,36 @@ package bluetooth.francesc.dam2.iesebre.com.bluetooth;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.widget.ViewAnimator;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 1;
-    private static final String TAG = "Bluetooth MainActivity";
-    private  BluetoothAdapter mBluetoothAdapter;
+    private static final String TAG = "MainActivity";
+    private BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth
-            Toast.makeText(MainActivity.this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(MainActivity.this, "Bluetooth is available", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Bluetooth is Available!", Toast.LENGTH_LONG).show();
         }
-
     }
 
     @Override
@@ -39,22 +44,67 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
         switch (requestCode) {
             case REQUEST_ENABLE_BT:
-                // When the request to enable Bluetooth returns
                 if (resultCode == Activity.RESULT_OK) {
                     // Bluetooth is now enabled, so set up a chat session
-                    //setupChat();
-                    Toast.makeText(MainActivity.this, "Bluetooth is available", Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "Bluetooth is active!", Toast.LENGTH_LONG).show();
                 } else {
                     // User did not enable Bluetooth or an error occurred
                     Log.d(TAG, "BT not enabled");
-                    Toast.makeText(this, "Bluetooth is not available",
+                    Toast.makeText(this, "Bluetooth no s'ha activat correctament!",
                             Toast.LENGTH_SHORT).show();
                     this.finish();
                 }
+                break;
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(
+            Menu menu) {
+        getMenuInflater().inflate(
+                R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(
+            MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.menu_discover:
+                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                startActivity(discoverableIntent);
+                return true;
+
+            case R.id.menu_paired:
+                Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+// If there are paired devices
+                if (pairedDevices.size() > 0) {
+                    // Loop through paired devices
+                    for (BluetoothDevice device : pairedDevices) {
+                        // Add the name and address to an array adapter to show in a ListView
+
+                        Toast.makeText(this, device.getName(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
